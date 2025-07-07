975ccb3cd6ccca2e6f6c8d47176ee0360e0a36a5
VikasKumarSingh <vikaskumarsingh0931@gmail.com>
2025-07-08 00:40:59 +0530

    Enhance Coming Soon page with new design; update layout, styles, and add social media links

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,29 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="coming-soon-container">
-      <div className="coming-soon-content">
-        <h1>🚀 Coming Soon</h1>
-        <p>We’re working hard to launch our website.<br />Stay tuned!</p>
-        <div className="loader"></div>
+    <div className="coming-soon-bg">
+      <div className="coming-soon-glass">
+        <h1 className="coming-soon-title">
+          <span>🚀</span>
+          <span className="gradient-text">We Are Coming Soon</span>
+        </h1>
+        <p className="coming-soon-desc">
+          We’re working hard to launch our website.<br />Stay tuned!
+        </p>
+        <div className="animated-loader"></div>
+        <div className="social-icons">
+          
+          <a href="https://www.instagram.com/shri_vikeshananda/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+            <svg width="28" height="28" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.334 3.608 1.308.974.974 1.246 2.241 1.308 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.334 2.633-1.308 3.608-.974.974-2.241 1.246-3.608 1.308-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.334-3.608-1.308-.974-.974-1.246-2.241-1.308-3.608C2.175 15.647 2.163 15.267 2.163 12s.012-3.584.07-4.85c.062-1.366.334-2.633 1.308-3.608C4.515 2.567 5.782 2.295 7.148 2.233 8.414 2.175 8.794 2.163 12 2.163zm0-2.163C8.741 0 8.332.013 7.052.072 5.771.131 4.659.392 3.678 1.373c-.98.98-1.242 2.092-1.301 3.373C2.013 8.332 2 8.741 2 12c0 3.259.013 3.668.072 4.948.059 1.281.321 2.393 1.301 3.373.98.98 2.092 1.242 3.373 1.301C8.332 23.987 8.741 24 12 24s3.668-.013 4.948-.072c1.281-.059 2.393-.321 3.373-1.301.98-.98 1.242-2.092 1.301-3.373.059-1.28.072-1.689.072-4.948s-.013-3.668-.072-4.948c-.059-1.281-.321-2.393-1.301-3.373-.98-.98-2.092-1.242-3.373-1.301C15.668.013 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zm0 10.162a3.999 3.999 0 1 1 0-7.998 3.999 3.999 0 0 1 0 7.998zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z"/></svg>
+          </a>
+          
+          <a href="https://www.facebook.com/people/Shri-Vikeshananda/100058855701048/?ref=_ig_profile_ac" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+            <svg width="28" height="28" fill="currentColor" viewBox="0 0 24 24"><path d="M22.675 0h-21.35C.597 0 0 .592 0 1.326v21.348C0 23.408.597 24 1.325 24h11.495v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.797.143v3.24l-1.918.001c-1.504 0-1.797.715-1.797 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116C23.403 24 24 23.408 24 22.674V1.326C24 .592 23.403 0 22.675 0"/></svg>
+          </a>
+          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+            <svg width="28" height="28" fill="currentColor" viewBox="0 0 24 24"><path d="M24 4.557a9.93 9.93 0 0 1-2.828.775 4.932 4.932 0 0 0 2.165-2.724c-.951.564-2.005.974-3.127 1.195A4.92 4.92 0 0 0 16.616 3c-2.73 0-4.942 2.21-4.942 4.936 0 .387.045.763.127 1.124C7.728 8.89 4.1 6.89 1.671 3.905a4.822 4.822 0 0 0-.666 2.482c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 0 1-2.237-.616v.062c0 2.388 1.7 4.384 3.946 4.834a4.936 4.936 0 0 1-2.224.084c.627 1.956 2.444 3.377 4.6 3.417A9.867 9.867 0 0 1 0 21.543a13.94 13.94 0 0 0 7.548 2.212c9.057 0 14.009-7.496 14.009-13.986 0-.213-.005-.425-.014-.636A9.936 9.936 0 0 0 24 4.557z"/></svg>
+          </a>
+        </div>
       </div>
       <footer className="coming-soon-footer">
         &copy; {new Date().getFullYear()} Rootline. All rights reserved.
